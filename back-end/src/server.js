207a113a1a5c4ad651a18dd73769a2abbb1384a3
Nithin-Tss <Nithin.B@techstalwartsolution.com>
2026-08-10 17:46:20 +0530

    feat: refactor server startup to include database connection

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,18 @@
 require("dotenv").config();
 
 const app = require("./app");
+const connectDB = require("./config/db");
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+async function start() {
+  await connectDB();
+  app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+  });
+}
+
+start().catch((err) => {
+  console.error("Failed to start server:", err.message);
+  process.exit(1);
 });
